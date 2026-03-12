6980b4b9bcf2a212b9748b3ddd3c1a7d0787c144
Ömer Yusuf Doğan <dogano20@itu.edu.tr>
2026-03-13 01:46:20 +0300

    Fix formatting and improve request handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,12 +40,12 @@ if ingredients_list :
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         
-        st.subheader(fruit_chosen + 'Nutrition Information') 
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")      
-        sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width = True)
+        st.subheader(fruit_chosen + ' Nutrition Information') 
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")      
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '"""+name_on_order+ """')"""
 
     #st.write(my_insert_stmt)
